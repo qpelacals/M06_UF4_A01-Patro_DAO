7b575611831eb09ad8a29f1b21a30cc864a6cc89
qpelacals <quipegol@gmail.com>
2025-02-04 19:02:22 +0100

    Primer commit

--- src/main/java/com/iticbcn/quimpelacals/Tasca.java
+++ src/main/java/com/iticbcn/quimpelacals/Tasca.java
@@ -1,14 +1,35 @@
 package com.iticbcn.quimpelacals;
 
-import java.sql.Date;
+import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.Date;
+
+@Entity
+@Table(name = "Tasca", uniqueConstraints = {
+        @UniqueConstraint(columnNames = "nom")
+})
 public class Tasca {
-    private Integer id;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nom", nullable = false)
     private String nom;
+
+    @Column(name = "descripcio", nullable = true)
     private String descripcio;
+
+    @Column(name = "prioritat", nullable = false)
     private String prioritat;
+
+    @Column(name = "data_lim", nullable = false)
+    @Temporal(TemporalType.DATE)
     private Date dataLim;
 
-    // Getters and Setters
-    // Constructors if necessary
+    @OneToMany(mappedBy = "tasca", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Historic> historics = new ArrayList<>();
+
+    // Getters i Setters
 }
